b640b37871ff7f29c1a53f77cb247dc0164ffc4a
tim.aeom <tim.aeom@kakaocorp.com>
2022-05-03 22:29:30 +0900

    precommit ..?

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -6,7 +6,17 @@ import org.junit.jupiter.api.Test
 class PersonTest {
 
     @Test
-    fun `널 타입`(      ) {
+    fun `이름 붙인 인자`(     ) {
+        val people = listOf(
+            Person("엄현식", 10, nickname = "tim.aeom"),
+            Person(name = "엄현식", age = 10, nickname = "tim.aeom")
+        )
+        assertThat(people[0].name).isEqualTo("엄현식")
+        assertThat(people[1].name).isEqualTo("엄현식")
+    }
+
+    @Test
+    fun `널 타입`() {
         val person = Person("엄현식", 10, null)
         assertThat(person.nickname).isNull()
     }
